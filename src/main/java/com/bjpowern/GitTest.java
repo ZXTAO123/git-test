846119f37a90dfb0c6676827058ee2e4dab09975
root <1095071050@qq.com>
2022-08-19 01:42:49 +0800

    second commit

--- src/main/java/com/bjpowern/GitTest.java
+++ src/main/java/com/bjpowern/GitTest.java
@@ -1,2 +1,8 @@
-package com.bjpowern;public class GitTest {
+package com.bjpowern;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("asda");
+    }
 }
